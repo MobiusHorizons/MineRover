b885e451e25a4694a3e567ec0b87921b1fdadc71
Paul martin <codemobius@gmail.com>
2013-08-13 19:15:19 -0400

    fixes in tile class

--- Mine/src/tile.java
+++ Mine/src/tile.java
@@ -9,11 +9,18 @@ import javax.imageio.ImageIO;
 public class tile {
 	BufferedImage sprite;
 	int value;
-	static String[] file_names = {"tunnel.png", "dirt.png", "iron.png", "gold.png", "silver.png", "diamond.png", "emerald.png"};
+	int fuelCost;
+	String name;
+	static String[] fileNames = {"tunnel.png", "dirt.png", "iron.png", "gold.png", "silver.png", "diamond.png", "emerald.png"};
+	static int[] moneyValues = {0,0,1,3,2,5,4};
+	static int[] fuelConsumption = {1,2,4,3,3,6,5};
 	public tile(Dimension size, int number){
+		value = moneyValues[number];
+		fuelCost = fuelConsumption[number];
+		name = fileNames[number].split(".")[0];
 		sprite = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
 		try {
-			sprite = ImageIO.read(new File("tiles/"+file_names[number]));
+			sprite = ImageIO.read(new File("tiles/"+fileNames[number]));
 		} catch (IOException e) {
 			e.printStackTrace();}
 	}
@@ -22,6 +29,12 @@ public class tile {
 	}
 	public int getValue() {
 		return value;
+	}
+	public int getFuelCost(){
+		return fuelCost;
+	}
+	public String getName(){
+		return name;
 	}
 	
 }
